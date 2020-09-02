1be8141d1f5d12c58c3a9c374a2b4fc3661a7ce0
Anselm <anselmleo@gmail.com>
2020-09-02 16:27:07 +0100

    setup user resend email verification

---
+++ models/emailverificationtoken.js
@@ -0,0 +1,14 @@
+
+const mongoose = require("mongoose");
+const config = require("config");
+
+
+const TokenSchema = new mongoose.Schema({
+  _userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
+  token: { type: String, required: true },
+  createdAt: { type: Date, required: true, default: Date.now, expires: 43200 }
+});
+
+const Token = mongoose.model("Token", TokenSchema);
+
+module.exports.Token = Token;
