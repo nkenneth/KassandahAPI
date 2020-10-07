02a25e6be42a8fe817fc5710eef5895033be2d66
Anselm <anselmleo@gmail.com>
2020-10-07 15:36:24 +0100

    .

--- services/response.js
+++ services/response.js
@@ -34,7 +34,7 @@ function error(res, msg = 'Error Occured', code = 400) {
 }
 
 
-function redirect(res, url = config.get("app_domain"), msg = null) {
+function redirect(res, url = config.get("app_domain"), msg = "") {
     return res.redirect(301, `${url}${msg}`);
 }
 
