096e3d20ecb9ab6932948a911e824b4e132bc39e
Migel <migellars@gmail.com>
2020-11-02 12:04:01 +0100

    ..

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -7,14 +7,27 @@ const response = require("../services/response");
 const router = express.Router();
 
 
-router.get("totalUsers",adminAuth, async, (req, res) => {
+router.get("totalActiveUsers",adminAuth, async, (req, res) => {
     try {
-        totalUser = await User.find({ isVerified : true});
-        var host = totalUser.count
-        return response.withData(res, host);
+        totalUser = await User.find({ isVerified : true, status: "active"});
+        var total_active_users = totalUser.count
+        return response.withData(res, total_active_users);
     } catch (error) {
         return response.error(res, error.message);
     }
 });
 
+router.get("totalPendingRequest", adminAuth, async (req, res) => {
+
+});
+
+router.get("totalUsers", adminAuth, async (req, res) =>{
+
+});
+
+router.get("totalDeclinedRequest", adminAuth, async (req, res) => {
+
+});
+
 module.exports = router;
+ 
