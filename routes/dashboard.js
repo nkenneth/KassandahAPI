3f0d7c676b3c126a7d91e79766443946691dcd42
Migel <migellars@gmail.com>
2020-11-02 15:57:22 +0100

    dashboard quickfix

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,6 +2,7 @@
 const config = require("config");
 const express = require("express");
 const { adminAuth } = require("../middleware/auth");
+const { Ticket } = require("../models/ticket");
 const { User } = require("../models/user");
 const response = require("../services/response");
 const router = express.Router();
@@ -9,7 +10,7 @@ const router = express.Router();
 
 router.get("totalActiveUsers",adminAuth, async, (req, res) => {
     try {
-        totalUser = await User.find({ isVerified : true, status: "approved"});
+        totalUser = await User.find({ isVerified : true, status: "active"});
         var total_active_users = totalUser.count()
         return response.withData(res, total_active_users);
     } catch (error) {
@@ -19,7 +20,7 @@ router.get("totalActiveUsers",adminAuth, async, (req, res) => {
 
 router.get("totalPendingRequests", adminAuth, async (req, res) => {
 try{
-    totalPendingRequest = await User.find({ status: "pending"});
+    totalPendingRequest = await Ticket.find({ status: "pending"});
     var total_pending_requests =  totalPendingRequest.count();
     return response.withData(res, total_pending_requests);
 } catch(error) {
@@ -39,7 +40,7 @@ try {
 
 router.get("totalDeclinedRequest", adminAuth, async (req, res) => {
     try{
-        totalDeclinedRequest = await User.find({ status: "declined"});
+        totalDeclinedRequest = await Ticket.find({ status: "rejected"});
         var total_declined_requests =  totalDeclinedRequest.count();
         return response.withData(res, total_declined_requests);
     } catch(error) {
