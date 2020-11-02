54e156147dab5d61e285dea418a402820602d824
Migel <migellars@gmail.com>
2020-11-02 12:21:20 +0100

    dashboard analytics endpoint.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -9,24 +9,42 @@ const router = express.Router();
 
 router.get("totalActiveUsers",adminAuth, async, (req, res) => {
     try {
-        totalUser = await User.find({ isVerified : true, status: "active"});
-        var total_active_users = totalUser.count
+        totalUser = await User.find({ isVerified : true, status: "approved"});
+        var total_active_users = totalUser.count()
         return response.withData(res, total_active_users);
     } catch (error) {
         return response.error(res, error.message);
     }
 });
 
-router.get("totalPendingRequest", adminAuth, async (req, res) => {
-
+router.get("totalPendingRequests", adminAuth, async (req, res) => {
+try{
+    totalPendingRequest = await User.find({ status: "pending"});
+    var total_pending_requests =  totalPendingRequest.count();
+    return response.withData(res, total_pending_requests);
+} catch(error) {
+    return response.error(res, error.message);
+}
 });
 
 router.get("totalUsers", adminAuth, async (req, res) =>{
-
+try {
+    totalUser = await User.find();
+    var total_users = totalUsers.count();
+    return response.withData(res, total_users);
+} catch (error) {
+    return response.error(res, error.message);
+}
 });
 
 router.get("totalDeclinedRequest", adminAuth, async (req, res) => {
-
+    try{
+        totalDeclinedRequest = await User.find({ status: "declined"});
+        var total_declined_requests =  totalDeclinedRequest.count();
+        return response.withData(res, total_declined_requests);
+    } catch(error) {
+        return response.error(res, error.message);
+    }
 });
 
 module.exports = router;
