59c96fd0659f98ff7f963069acfe6e6121a04e25
Anselm <anselmleo@gmail.com>
2020-11-02 11:14:47 +0100

    expand file type validation

--- services/fileService.js
+++ services/fileService.js
@@ -2,7 +2,7 @@
 
 const documentFilter = function(req, file, cb) {
   // Accept images, pdf and word documents only
-  if (file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF|pdf|PDF|doc|DOC|docx|DOCX)$/)) {
+  if (file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF|pdf|PDF|doc|DOC|docx|DOCX|xls|XLS|xlsx|XLSX)$/)) {
     return cb(null, true);
   } 
   req.fileValidationError = 'Only images, pdf and word documents are allowed!';
